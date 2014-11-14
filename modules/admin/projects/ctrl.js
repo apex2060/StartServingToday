d61c7cce183fa56056b0319cf7d7af0edaa64321
Ryan Quinlan <apex2060@site4u2.com>
2014-11-14 19:58:15 +0000

    Removed old project files

--- modules/admin/projects/ctrl.js
+++
@@ -1,6 +0,0 @@
-var ProjectCtrl = app.lazy.controller('ProjectCtrl', function($rootScope, $scope, $http, $q, config, projectService){
-	
-	$scope.tools = projectService;
-	it.ProjectCtrl=$scope;
-	
-});
